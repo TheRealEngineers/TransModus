be28ca68e02e7f3aa695c789a1455ac33f6a56f3
Sebastian Ibarra <136394664+SebasIbCS@users.noreply.github.com>
2023-06-15 14:12:20 -0700

    removed uneeded package

--- .idea/LoginForm.java
+++ .idea/LoginForm.java
@@ -4,8 +4,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-package loginsystem;
-
 public class LoginForm extends javax.swing.JFrame {
     // Creates new form LoginForm
     public LoginForm() {
