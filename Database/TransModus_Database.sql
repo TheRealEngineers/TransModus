a94025cf40df06837004fc72608cdb453b3281ca
Chance Chime <cchime82@icloud.com>
2023-06-15 13:56:53 -0700

    TRS-30 Testing Editing of Database

--- Database/TransModus_Database.sql
+++ Database/TransModus_Database.sql
@@ -10,38 +10,49 @@ CREATE TABLE IF NOT EXISTS client (
   `client_id` INT(9) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(50) NOT NULL,
   `login` VARCHAR(50) NOT NULL,
+  `email` VARCHAR(50) DEFAULT NULL,
   `password` VARCHAR(50) NOT NULL,
   `birthday_month` CHAR(3) NOT NULL,
   `birthday_day` INT(2) NOT NULL,
   `birthday_year` INT(4) NOT NULL
 );
 
+ALTER TABLE client AUTO_INCREMENT = 1;
 
 -- Insert data into the table
-INSERT INTO client (name, login, password, birthday_month, birthday_day, birthday_year)
-VALUES ('John Doe', 'john.doe@example.com', 'biscuitwarrior', 'Jan', 01, 2000),
-       ('Jane Smith', 'jane.smith@example.com', 'justiceandliberty123', 'Aug', 18, 2002),
-       ('Mike Johnson', 'mike.johnson@example.com', 'password123', 'Apr', 28, 2003);
+INSERT INTO client (name, login, email, password, birthday_month, birthday_day, birthday_year)
+VALUES ('John Doe', 'jdoe1', 'john.doe@example.com', 'biscuitwarrior', 'Jan', 01, 2000),
+       ('Jane Smith', 'jsmith2', 'jane.smith@example.com', 'justiceandliberty123', 'Aug', 18, 2002),
+       ('Mike Johnson','mjohnson3', 'mike.johnson@example.com', 'password123', 'Apr', 28, 2003);
 
 -- Remove duplicates from the table
 DELETE c1 FROM client c1
-                   JOIN
-               client c2 ON c1.name = c2.name
-                   AND c1.login = c2.login
+               JOIN
+               client c2 ON c1.client_id > c2.client_id
+               WHERE
+                   c1.client_id = c2.client_id
+                   AND c1.name = c2.name
+                   OR c1.login = c2.login
+                   OR c1.email = c2.email
                    AND c1.birthday_month = c2.birthday_month
                    AND c1.birthday_day = c2.birthday_day
-                   AND c1.birthday_year = c2.birthday_year
-WHERE
-        c1.client_id > c2.client_id;
-
+                   AND c1.birthday_year = c2.birthday_year;
+-- WHERE
+   --     c1.client_id > c2.client_id;
+-- Will remove the users without email?
+DELETE FROM client WHERE email = '';
 -- Retrieve data from the table
+
 SELECT
     LPAD(client_id, 5, '0') AS formatted_id,
     name,
-    login,
+    login AS username,
+    email,
     password,
     birthday_month,
     birthday_day,
     birthday_year
 FROM
-    client;
+    client
+WHERE
+    email <> '';
