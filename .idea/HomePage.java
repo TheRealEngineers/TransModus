e846639c7056ad4dc5201cb62fdfa3bc63501a94
Sebastian Ibarra <136394664+SebasIbCS@users.noreply.github.com>
2023-06-15 00:22:06 -0700

    Update HomePage.java

--- .idea/HomePage.java
+++ .idea/HomePage.java
@@ -1,14 +1,3 @@
-package loginsystem;
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-
-/**
- *
- * @author Andres Ibarra
- */
 public class HomePage extends javax.swing.JFrame {
 
     /**
