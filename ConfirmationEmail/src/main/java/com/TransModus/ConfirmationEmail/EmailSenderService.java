88f2fd8dad3dbc902457d42b0bd18e2660088074
David Pedroza <107772031+Zyoom@users.noreply.github.com>
2023-06-27 21:19:02 -0700

    Add files via upload
    Extra Files for the Email Confirmation code ;)

---
+++ ConfirmationEmail/src/main/java/com/TransModus/ConfirmationEmail/EmailSenderService.java
@@ -0,0 +1,32 @@
+package com.TransModus.ConfirmationEmail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService {
+    @Autowired
+    private JavaMailSender mailSender;
+    public void sendEmail(String toEmail,
+                          String subject,
+                          String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("jaikoisal@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(String.valueOf(message));
+
+        mailSender.send(message);
+
+        System.out.println("Mail Sent successfully...");
+
+    }
+}
+/*
+For Application Properties we need to add an EMAIL AND PASSWORD under the password and username section this we
+will use either Bhance Bhim to get this going I already tried making one for TransModus but couldn't complete it as
+I did not have another email or phone number to associate this email sender too so this is why we prefer to use
+you Bhance Bhime : )
+ */
---
+++ ConfirmationEmail/src/test/java/com/TransModus/ConfirmationEmail/ConfirmationEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TransModus.ConfirmationEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfirmationEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
