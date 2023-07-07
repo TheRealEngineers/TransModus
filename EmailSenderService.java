80caaad91d6cee4e7da0f5b8a625aa5f2fee930b
David Pedroza <107772031+Zyoom@users.noreply.github.com>
2023-07-07 15:23:58 -0700

    Updated EmailSenderService.java
    now it will send a the reviews to the email address

--- EmailSenderService.java
+++ EmailSenderService.java
@@ -1,5 +1,7 @@
 package com.TransModus.ConfirmationEmail;
 
+import javax.swing.JOptionPane;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -23,6 +25,26 @@ public class EmailSenderService {
         System.out.println("Mail Sent successfully...");
 
     }
+    private void btnSendReviewActionPerformed(java.awt.event.ActionEvent evt) {
+    // Get the user's review text from the jTextField1 field
+    String reviewText = jTextField1.getText();
+    
+    // Create an instance of the EmailSenderService
+    EmailSenderService emailSenderService = new EmailSenderService();
+    
+    // Set the email details
+    String toEmail = "jaikoisal@gmail.com"; // Set the recipient email address
+    String subject = "Review Feedback"; // Set the email subject
+    String body = "Review Text: " + reviewText; // Set the email body
+    
+    // Send the email
+    emailSenderService.sendEmail(toEmail, subject, body);
+    
+    // Show a confirmation message to the user
+    JOptionPane.showMessageDialog(this,
+        "Your review has been sent! Thank you for your feedback!");
+}
+
 }
 /*
 For Application Properties we need to add an EMAIL AND PASSWORD under the password and username section this we
